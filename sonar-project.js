fa4d665ac798a75bf6d0d6e1d6ffa847373fe71e
Sowmya241220 <gundetisowmya05@gmail.com>
2025-01-18 14:17:50 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.235.115.2:9000/',
+  serverUrl: 'http://172.31.38.249:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
